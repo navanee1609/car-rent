3590f0af9c8d7d843d1774279b7130c0d8ed3df6
navanee1609 <navaneethanvs18@gmail.com>
2023-10-26 15:20:11 +0530

    completed header section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { Col, Container, Row } from 'reactstrap'
+
 
 const Header = () => {
   return <header className='header'>
@@ -19,9 +21,17 @@ const Header = () => {
               </div>
             </Col>
 
-          <Col lg='6' md='6' sm='6'>
-            
-          </Col>
+            <Col lg="6" md="6" sm="6">
+              <div className="header__top__right d-flex align-items-center justify-content-end gap-3">
+                <Link to="#" className=" d-flex align-items-center gap-1">
+                  <i class="ri-login-circle-line"></i> Login
+                </Link>
+
+                <Link to="#" className=" d-flex align-items-center gap-1">
+                <i class="ri-user-add-line"></i> Register
+                </Link>
+              </div>
+            </Col>
         </Row>
       </Container>
 
