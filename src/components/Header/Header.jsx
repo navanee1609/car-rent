c6b5bb4888f1a5e10f646a614f5d071eebd71bd9
navanee1609 <navaneethanvs18@gmail.com>
2023-10-26 15:15:52 +0530

    added need help section in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import { Col, Container, Row } from 'reactstrap'
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
+  return <header className='header'>
+
+
+    {/* header top */}
+
+    <div className='header__top'>
+      <Container>
+        <Row>
+        <Col lg="6" md="6" sm="6">
+              <div className="header__top__left">
+                <span>Need Help?</span>
+                <span className="header__top__help">
+                  <i class="ri-phone-fill"></i> +12-345-67890
+                </span>
+              </div>
+            </Col>
+
+          <Col lg='6' md='6' sm='6'>
+            
+          </Col>
+        </Row>
+      </Container>
+
+    </div>
+  </header>
 }
 
 export default Header
