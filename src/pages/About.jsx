20809af1f131e7de2ff3d7465800e6c674530a51
navanee1609 <navaneethanvs18@gmail.com>
2023-10-27 21:50:12 +0530

    added about section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Container, Row, Col } from "reactstrap";
+
 
 const About = () => {
-  return (
-    <div>About</div>
-  )
+  return <section>
+    <container>
+      <Row>
+        <col lg='6' md='6'></col>
+
+        
+        <col lg='6' md='6'></col>
+      </Row>
+    </container>
+  </section>
 }
 
 export default About
