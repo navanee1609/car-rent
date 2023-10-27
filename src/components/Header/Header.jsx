b61699e8367d0b92ce78bbc9e7d2d78ea634f31e
navanee1609 <navaneethanvs18@gmail.com>
2023-10-27 19:10:17 +0530

    added function to navbar, completed header section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,50 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { Col, Container, Row } from 'reactstrap'
-import '../../styles/header.css'
+import React, { useRef } from "react";
+
+import { Container, Row, Col } from "reactstrap";
+import { Link, NavLink } from "react-router-dom";
+import "../../styles/header.css";
 
 
+
+// links to get redirect
+const navLinks = [
+  {
+    path: "/home",
+    display: "Home",
+  },
+  {
+    path: "/about",
+    display: "About",
+  },
+  {
+    path: "/cars",
+    display: "Cars",
+  },
+
+  {
+    path: "/blogs",
+    display: "Blog",
+  },
+  {
+    path: "/contact",
+    display: "Contact",
+  },
+];
+
+
+// header section
 const Header = () => {
-  return <header className='header'>
+  const menuRef = useRef(null);
 
+  const toggleMenu = () => menuRef.current.classList.toggle("menu__active");
 
-    {/* header top */}
-
-    <div className='header__top'>
-      <Container>
-        <Row>
-        <Col lg="6" md="6" sm="6">
+  return (
+    <header className="header">
+      {/* ============ header top ============ */}
+      <div className="header__top">
+        <Container>
+          <Row>
+            <Col lg="6" md="6" sm="6">
               <div className="header__top__left">
                 <span>Need Help?</span>
                 <span className="header__top__help">
@@ -29,20 +60,16 @@ const Header = () => {
                 </Link>
 
                 <Link to="#" className=" d-flex align-items-center gap-1">
-                <i class="ri-user-add-line"></i> Register
+                  <i class="ri-user-line"></i> Register
                 </Link>
               </div>
             </Col>
-        </Row>
-      </Container>
+          </Row>
+        </Container>
+      </div>
 
-    </div>
-
-
-
-    {/* header middle */}
-
-    <div className="header__middle">
+      {/* =============== header middle =========== */}
+      <div className="header__middle">
         <Container>
           <Row>
             <Col lg="4" md="3" sm="4">
@@ -58,8 +85,6 @@ const Header = () => {
               </div>
             </Col>
 
-            {/* location */}
-
             <Col lg="3" md="3" sm="4">
               <div className="header__location d-flex align-items-center gap-2">
                 <span>
@@ -72,8 +97,6 @@ const Header = () => {
               </div>
             </Col>
 
-
-            {/* timing */}
             <Col lg="3" md="3" sm="4">
               <div className="header__location d-flex align-items-center gap-2">
                 <span>
@@ -86,9 +109,6 @@ const Header = () => {
               </div>
             </Col>
 
-
-
-            {/* register/login */}
             <Col
               lg="2"
               md="3"
@@ -104,7 +124,46 @@ const Header = () => {
           </Row>
         </Container>
       </div>
-  </header>
-}
 
-export default Header
+      {/* ========== main navigation =========== */}
+
+      <div className="main__navbar">
+        <Container>
+          <div className="navigation__wrapper d-flex align-items-center justify-content-between">
+            <span className="mobile__menu">
+              <i class="ri-menu-line" onClick={toggleMenu}></i>
+            </span>
+
+            <div className="navigation" ref={menuRef} onClick={toggleMenu}>
+              <div className="menu">
+                {navLinks.map((item, index) => (
+                  <NavLink
+                    to={item.path}
+                    className={(navClass) =>
+                      navClass.isActive ? "nav__active nav__item" : "nav__item"
+                    }
+                    key={index}
+                  >
+                    {item.display}
+                  </NavLink>
+                ))}
+              </div>
+            </div>
+
+{/* navbar right */}
+            <div className="nav__right">
+              <div className="search__box">
+                <input type="text" placeholder="Search" />
+                <span>
+                  <i class="ri-search-line"></i>
+                </span>
+              </div>
+            </div>
+          </div>
+        </Container>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
