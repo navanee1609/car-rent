57680b289ae2ee493f698957f69313272fc61b09
navanee1609 <navaneethanvs18@gmail.com>
2023-10-27 21:41:26 +0530

    removed comment for find-car.css

--- src/components/UI/FindCarForm.jsx
+++ src/components/UI/FindCarForm.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-// import "../../styles/find-car-form.css";
+import "../../styles/find-car-form.css";
 import "../../styles/find-car-form.css";
 import { Form, FormGroup } from "reactstrap";
 
@@ -9,7 +9,7 @@ const FindCarForm = () => {
       <div className=" d-flex align-items-center justify-content-between flex-wrap">
         <FormGroup className="form__group">
           <input type="text" placeholder="From address" required />
-        </FormGroup>
+        </FormGroup> 
 
         <FormGroup className="form__group">
           <input type="text" placeholder="To address" required />
