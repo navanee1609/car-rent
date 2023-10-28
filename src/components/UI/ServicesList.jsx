0b915dfe092835a9e7b8467dc6f64d8492d1b599
navanee1609 <navaneethanvs18@gmail.com>
2023-10-28 18:48:50 +0530

    added service list css in corresponding jsx file

--- src/components/UI/ServicesList.jsx
+++ src/components/UI/ServicesList.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Col } from "reactstrap";
-// import "../../styles/services-list.css";
+import "../../styles/services-list.css";
 import servicesData from "../../assets/data/serviceData";
 
 const ServicesList = () => {
