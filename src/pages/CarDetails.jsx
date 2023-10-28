7716498012a686e7738dc7ff6ccb79d2154f7f80
navanee1609 <navaneethanvs18@gmail.com>
2023-10-28 20:23:37 +0530

    added booking form to car details page

--- src/pages/CarDetails.jsx
+++ src/pages/CarDetails.jsx
@@ -4,7 +4,7 @@ import carData from "../assets/data/carData";
 import { Container, Row, Col } from "reactstrap";
 import Helmet from "../components/Helmet/Helmet";
 import { useParams } from "react-router-dom";
-// import BookingForm from "../components/UI/BookingForm";
+import BookingForm from "../components/UI/BookingForm";
 // import PaymentMethod from "../components/UI/PaymentMethod";
 
 const CarDetails = () => {
@@ -110,7 +110,7 @@ const CarDetails = () => {
             <Col lg="7" className="mt-5">
               <div className="booking-info mt-5">
                 <h5 className="mb-4 fw-bold ">Booking Information</h5>
-                {/* <BookingForm /> */}
+                <BookingForm />
               </div>
             </Col>
 
